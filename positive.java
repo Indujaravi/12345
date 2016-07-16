eb5dde4940fb4579f7dc532376e4731bc8bfcc7a
Induja <rindhuja25@gmail.com>
2016-07-16 10:36:06 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,8 @@
+Scanner sc=new Scanner(System.in)
+int in=sc.nextInt();
+if(in>0)
+  System.out.println("positive"+in);
+else if(in<0)
+  System.out.println("ngative"+in);
+else if(in==0)
+  System.out.println("zero"+in);
