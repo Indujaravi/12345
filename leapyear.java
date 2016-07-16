d94e623b21290630ebbc2198950816537875fe9f
Induja <rindhuja25@gmail.com>
2016-07-16 10:52:28 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,7 @@
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+ if(n1%4==0||n1%400==0||n1%100==0)
+    System.out.println("leapyear");
+ else
+    System.out.println("not leapyear");
+    
