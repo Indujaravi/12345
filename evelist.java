08974a24fb81c1e74a48102771d736bea6b2b8e6
Induja <rindhuja25@gmail.com>
2016-07-16 17:20:46 +0530

    Add files via upload

---
+++ evelist.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int no=sc.nextInt();
+        int i;
+        for(i=1;i<=no;i++)
+        {
+         if(i%2==0)
+                System.out.println("no are"+i);
+        }
+    }
+    
