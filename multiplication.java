79a5ac8cf402b10b5577484b16a08a851dde5c95
Induja <rindhuja25@gmail.com>
2016-07-18 09:47:27 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n1=s.nextInt();
+        int i,n;
+        for(i=1;i<=10;i++){
+            n=i*n1;
+            System.out.println(""+i+"*"+n1+"="+n);
+        }
+    }
