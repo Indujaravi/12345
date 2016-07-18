a1ed3fe0b563bc5dc0b263e8ab9d84402c5f6a13
Induja <rindhuja25@gmail.com>
2016-07-18 09:21:04 +0530

    Add files via upload

---
+++ oddlimit.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        for(int i=n1;n1<=n2;n1++)
+        {
+            if(n1%2!=0)
+                System.out.println("odd no"+n1);
+    }}
