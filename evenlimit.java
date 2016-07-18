c3298e9b3bc9987526987d8c4b3144d6566d989e
Induja <rindhuja25@gmail.com>
2016-07-18 09:25:13 +0530

    Add files via upload

---
+++ evenlimit.java
@@ -0,0 +1,10 @@
+ public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        for(int i=n1;n1<=n2;n1++)
+        {
+            if(n1%2==0)
+                System.out.println("even no"+n1);
+    }
+    }
