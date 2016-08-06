074c76b35c995a1ea616f0b38e19d83eaf9961b1
Induja <rindhuja25@gmail.com>
2016-08-06 17:15:31 +0530

    Add files via upload

---
+++ powerno.java
@@ -0,0 +1,14 @@
+
+public class powno {
+    public static void main(String[] args) {
+        int n1,n2;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter no:");
+        n1=sc.nextInt();
+        System.out.println("enter power:");
+        n2=sc.nextInt();
+        System.out.println(Math.pow(n1,n2));
+
+    }
+    
+}
