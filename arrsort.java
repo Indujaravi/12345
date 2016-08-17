9a1a4dac321100d8bf4971f687d113a632ded62c
Induja <rindhuja25@gmail.com>
2016-08-17 12:36:13 +0530

    Add files via upload

---
+++ arrsort.java
@@ -0,0 +1,6 @@
+int iArr[] = {2, 1, 9, 6, 4};
+   for (int number : iArr) {
+   System.out.println("Number = " + number);
+   }   
+   Arrays.sort(iArr);   
+   System.out.println("The sorted int array is:");
