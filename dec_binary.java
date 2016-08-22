3463d3dff5aad6cb42f00c2bcf324a50152f617c
Induja <rindhuja25@gmail.com>
2016-08-22 18:22:30 +0530

    Add files via upload

--- dec_binary.java
+++ dec_binary.java
@@ -1,52 +1,35 @@
-Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+int n=sc.nextInt();
         int[] t=new int[10];
         int[] b=new int[10];
         String[] t1=new String[100];
         int n1,sum=0,count=0,c;
         String t2;
         for(int i=0;i<=n;i++)
-        {
-            while(n!=0)
-            {
-            t[i]=n%10;
+        {while(n!=0)
+           { t[i]=n%10;
             t1[i]=Integer.toBinaryString(t[i]);
                 //System.out.println(" "+t1[i]+" "+t[i]);
                 c=0;
                 int k=t1[i].length();
                 for(int j=0;j<k;j++)
-                {
-                   char c1=t1[i].charAt(j);
+                {char c1=t1[i].charAt(j);
                     //System.out.println(" "+c1);
                    if(c1=='1'){
                    c++;
-                   }
-                }
+                   }}
             b[i]=c;
             n=n/10;
-            }
-         }  
+            }}  
         for(int i=0;i<n;i++)
         {for(int j=i+1;j<n;j++)
-        {
-            if(b[i]<b[j])
-            {
-                t2=t1[i];
+        {if(b[i]<b[j])
+            {t2=t1[i];
                 t1[i]=t1[j];
                 t1[j]=t2;
             }
             if(b[i]==b[j])
-            {
-                if(b[i]<b[j])
-                {
-                     t2=t1[i];
+            {if(b[i]<b[j])
+                {t2=t1[i];
                 t1[i]=t1[j];
                 t1[j]=t2;
-            
-                }
-            }
-        }
-
-        }
-    }
-}
+      }}}}}}
