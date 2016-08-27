773a15a4f4f33274c6dc4e4f0522d5c15e644aba
Induja <rindhuja25@gmail.com>
2016-08-27 10:20:33 +0530

    Add files via upload

---
+++ sumofpow.java
@@ -0,0 +1,19 @@
+Scanner sc=new Scanner(System.in);
+        String no=sc.next();
+        int ln=no.length();
+        int sum=0,val;
+        int[] n1=new int[ln];
+        char[] ch=new char[ln];
+        ch=no.toCharArray();
+        for(int i=0;i<ln;i++){
+              n1[i]=ch[i]-48;   //System.out.println(" "+n1[i]);
+        }//System.out.println(" "+n1);
+        for(int i=0;i<=ln;i++)
+        {
+            int n2=n1[i];
+            int n=n1[i+1];
+            //System.out.println(" "+n1[i]+" "+n1[i+1]);
+            val=(int)Math.pow(n2, n);
+            sum=sum+val;
+            System.out.println(" "+sum);
+        }
