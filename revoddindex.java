3ae682457eda9de1f6fdb24fc600be4a96d138fc
Induja <rindhuja25@gmail.com>
2016-08-30 09:35:30 +0530

    Add files via upload

---
+++ revoddindex.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String str1[]=str.split("\\s");
+       // String str2[]=new String[str/2];
+        for(int i=0;i<str.length()-1;i=i+2)
+        {
+            
+            String str2=new StringBuffer(str1[i]).reverse().toString();
+            System.out.print(" "+str2);
+            
+        }
